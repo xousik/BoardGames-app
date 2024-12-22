168f49e0182d194a763d1df5dacc23708b94a261
xousik <patrykglinka2@gmail.com>
2024-12-22 17:05:23 +0000

    Initial commit, add 'Montserrat Aletrantes' as global font, creates LandingPageComponent.

---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'landing-page',
+  imports: [],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.css'
+})
+export class LandingPageComponent {
+}
